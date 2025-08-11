f950ecdc6ceb41efd0f98c7067fdb1b3550c5bdd
armohanyan <ohanyyanarmen@gmail.com>
2025-08-11 23:51:48 +0400

    fix feedbacks

--- client/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import request from '@/plugins/axios-interceptors/global.interceptor';
-function getRecentProducts(params) {
-    return request({
-        url: '/products',
-        params,
-    });
-}
-export { getRecentProducts };
-//# sourceMappingURL=index.js.map
